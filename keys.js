0927062302d3638936e7f25a144928994e67b01b
BenHigh <ben.high@austin.rr.com>
2017-10-30 22:25:45 -0500

    init base files

---
+++ keys.js
@@ -0,0 +1,11 @@
+
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'HRyDsyWYUByxYWgfdueKXRpwq',
+  consumer_secret: 'I5jH7lJb0DMHP0XkP8SEUtKjLyL0HyThPiPUITqyo7F4sWVGB2',
+  access_token_key: '925191630650593280-HrheljtMrG57pZ3cSpmNpkJxRoiLzEH',
+  access_token_secret: 'Y6YJZmY9caeEbaLGzZYUg9BHre3PWVnSLw4YIMfG5hI4T',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,10 @@
+var twit = require("twitter");
+
+var spot = require("node-spotify-api");
+
+var request = require("request");
+
+var keys = require("./keys.js");
+
+console.log(keys);
+
