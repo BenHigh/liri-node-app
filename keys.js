2486ae18bcb251ff0c2c818e43f05b5a8b205893
BenHigh <ben.high@austin.rr.com>
2017-11-02 10:09:39 -0500

    remove keys

--- keys.js
+++ keys.js
@@ -2,10 +2,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: 'RMewHTWI9DLNlnBbxOyCBp0Tt',
-  consumer_secret: 'hmLcT5ZgOGirlc33Xy9WRO6vKR2TyrP1UA8wahcVzfFKtVsEra',
-  access_token_key: '925191630650593280-HrheljtMrG57pZ3cSpmNpkJxRoiLzEH',
-  access_token_secret: 'Y6YJZmY9caeEbaLGzZYUg9BHre3PWVnSLw4YIMfG5hI4T'
+  consumer_key: 'XXXX',
+  consumer_secret: 'XXXX',
+  access_token_key: 'XXXX',
+  access_token_secret: 'XXXXs'
 }
 
 module.exports = twitterKeys;
